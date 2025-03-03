6ef45e22b7dbaf0d169552d04166f1d214f580d5
PingHungC <pinghung_chen@asus.com>
2025-03-03 16:41:57 +0800

    HW Test

--- student_assignment.py
+++ student_assignment.py
@@ -59,11 +59,57 @@ def generate_hw01():
             documents=df["HostWords"].fillna("").tolist()
         )
         
-    print("資料已成功存入 ChromaDB")
+    #print("資料已成功存入 ChromaDB")
     return collection
     
 def generate_hw02(question, city, store_type, start_date, end_date):
-    pass
+    try:
+        client = chromadb.PersistentClient(path=dbpath)
+        openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+            api_key = gpt_emb_config['api_key'],
+            api_base = gpt_emb_config['api_base'],
+            api_type = gpt_emb_config['openai_type'],
+            api_version = gpt_emb_config['api_version'],
+            deployment_id = gpt_emb_config['deployment_name']
+        )
+        collection = client.get_collection(
+            name="TRAVEL",
+            embedding_function=openai_ef
+        )
+        # 查詢 ChromaDB
+        results = collection.query(
+            query_texts=[question],
+            n_results=150
+        )
+
+        # print(results)
+
+        filtered_results = []
+        for i, score in enumerate(results["distances"][0]):
+            metadata = results["metadatas"][0][i]
+            # 篩選條件
+            
+            # if score < 0.80:
+            #     continue
+            if city and metadata["city"] not in city:
+                continue
+            if store_type and metadata["type"] not in store_type:
+                 continue
+            if start_date and metadata["date"] < int(start_date.timestamp()):
+                continue
+            if end_date and metadata["date"] > int(end_date.timestamp()):
+                continue
+            filtered_results.append((metadata["name"], score))
+        
+        # 按照相似度排序並提取名稱
+        filtered_results.sort(key=lambda x: x[1], reverse=True)
+        return [name for name, _ in filtered_results]
+        pass
+    
+    except Exception as e:
+        print("查詢失敗：")
+        traceback.print_exc()
+        return []
     
 def generate_hw03(question, store_name, new_store_name, city, store_type):
     pass
@@ -85,4 +131,12 @@ def demo(question):
     
     return collection
 
-#generate_hw01()
+
+# question = "我想要找有關茶餐點的店家"
+# city = ["宜蘭縣", "新北市"]
+# store_type = ["美食"]
+# start_date = datetime.datetime(2024, 4, 1)
+# end_date = datetime.datetime(2024, 5, 1)
+# generate_hw02(question, city, store_type, start_date, end_date)
+# results = generate_hw02(question, city, store_type, start_date, end_date)
+# print(results)
